5a8ee2ad79964cedfcebd4bfa53768db35edd4d6
tanjeeb ahsan <tanjeeb@cybernetikz.com>
2022-12-20 18:41:51 +0600

    create user list page

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class PagesController extends Controller
@@ -9,5 +10,11 @@ class PagesController extends Controller
     public function index()
     {
         return view('index');
+    }
+
+    public function users()
+    {
+        $users=User::all();
+        return view('user-list',['users'=>$users]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/',[PagesController::class,'index'])->name('home');
+Route::get('/user-list',[PagesController::class,'users'])->name('user');
